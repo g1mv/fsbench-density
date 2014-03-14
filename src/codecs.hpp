37ba0ed7ff8f5a6a0ebf154f9cce7ffe8c31c2b0
m <m>
2014-03-14 17:39:50 +0000

    Updated LZ4 to r114

--- src/codecs.hpp
+++ src/codecs.hpp
@@ -188,7 +188,7 @@ Codec * find_codec(const std::string & name);
 #define _HALIBUT_VERSION    "SVN r9550"
 #define _KECCAK_VERSION     "3.2"
 #define _LODEPNG_VERSION    "20120729"
-#define _LZ4_VERSION        "r97"
+#define _LZ4_VERSION        "r114"
 #define _LZF_VERSION        "3.6"
 #define _LZFX_VERSION       "r16"
 #define _LZG_VERSION        "1.0.6"
